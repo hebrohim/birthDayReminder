c3cb539b4b62011a9a8ac0b71c00803f789c35d7
hebrohim <raheemibrohim@mail.com>
2023-03-10 22:56:57 +0100

    feat: add media Query to make the application responsive on a smaller screen size

--- src/Components/ListPersons.js
+++ src/Components/ListPersons.js
@@ -3,17 +3,7 @@ import React, { useState } from "react";
 import "../index.css";
 const ListPersons = ({ people }) => {
 
-    let imageStyling = {
-        objectFit:"cover",
-        // paddingTop:"20px",
-        marginBottom:"20px",
-        width:"80px",
-        height:"80px",
-        borderRadius:"50%",
-        border:"2px solid #4d0801",
-        boxShadow:" 5px 3px 23px -4px rgba(0,0,0,0.76"
-
-    }
+   
   return (
     <div className="listContainer">
       {/* {console.log(people)} */}
@@ -21,10 +11,10 @@ const ListPersons = ({ people }) => {
         let { id, name, age, image } = person;
         return (
         <div key={id}  className ="userContainer">
-            <img src={image} style ={imageStyling}/>
+            <img id = "userImg"src={image} />
            
                 <div style={{marginLeft:"20px"}}>
-            <h4 style={{marginBottom:"10px",letterSpacing:"0.3rem",fontSize:"1.5rem"}}>{name}</h4>
+            <h4 id = "userName">{name}</h4>
 
             <p style={{fontWeight:"200",marginTop:"-20px"}}>{age} years</p>
         </div>
